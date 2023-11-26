5319ebe8fa08ef7cd01bc810db87d176b438bd38
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2023-11-26 11:21:23 +0300

    optimization

--- models/contacts.js
+++ models/contacts.js
@@ -1,70 +1,72 @@
 const fs = require("fs/promises");
 const path = "models/contacts.json";
 
-const genId = ()=>{
+const genId = () => {
   let id = "";
-  for (let i = 0; i < 21; i++) {  
-    let ch = String.fromCharCode(65+Math.floor(Math.random() * 25));
-    if(Math.random()>0.5){
-      ch=ch.toLowerCase();
+  for (let i = 0; i < 21; i++) {
+    let ch = String.fromCharCode(65 + Math.floor(Math.random() * 25));
+    if (Math.random() > 0.5) {
+      ch = ch.toLowerCase();
     }
-    id+=ch;    
+    id += ch;
   }
   return id;
-}
-const saveContact = async(id, name, email, phone)=>{
-  const res=  { id, name, email, phone }
-  fs.writeFile(
-    path,
-    JSON.stringify([... (await listContacts()),res])
-  );
+};
+const saveContact = async (id, name, email, phone) => {
+  const res = { id, name, email, phone };
+  const contacts = await listContacts();
+  contacts.push(res);
+  fs.writeFile(path, JSON.stringify(contacts));
   return res;
-}
+};
 
 const listContacts = async () => {
   return JSON.parse((await fs.readFile(path)).toString());
 };
 
 const getContactById = async (id) => {
-  return listContacts().then((v) => {
-    let cont;
-    v.forEach((contact) => {
-      if (contact.id === id) {
-        cont = contact;
-      }
-    });
-    return cont;
-  });
+  const contacts = await listContacts();
+  return contacts.find((c) => c.id === id);
 };
 
 const removeContact = async (contactId) => {
-  const toRem = await getContactById(contactId);
-  if(toRem===undefined){
-    return false;
+  const old = await listContacts();
+  const current = old.filter((v) => v.id !== contactId);
+  if (old.length !== current.length) {
+    fs.writeFile(path, JSON.stringify(current));
+    return true;
   }
-  fs.writeFile(
-    path,
-    JSON.stringify( (await listContacts()).filter(v=>v.id!==contactId))
-  );
-  return true;
+  return false;
 };
 
 const addContact = async ({ name, email, phone }) => {
- return await saveContact(genId(), name, email, phone);
+  return await saveContact(genId(), name, email, phone);
 };
 const updateContact = async (contactId, body) => {
   const guy = await getContactById(contactId);
-  if(guy===undefined){
+  if (guy === undefined) {
     return false;
   }
-  const {id=contactId, name = guy.name, email=guy.email, phone=guy.phone} = body;
-  
+  const {
+    id = contactId,
+    name = guy.name,
+    email = guy.email,
+    phone = guy.phone,
+  } = body;
+
+  const contacts = await listContacts();
+  contacts.forEach((c, i) => {
+    if(c.id===contactId){
+      contacts[i] =  { id, name, email, phone };
+    }
+  });
+
   fs.writeFile(
     path,
-    JSON.stringify( [...(await listContacts()).filter(v=>v.id!==contactId), {id, name, email, phone}])
+    JSON.stringify(contacts)
   );
 
-  return {id, name, email, phone};
+  return { id, name, email, phone };
 };
 
 module.exports = {
